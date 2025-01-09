430a70828e344a45b53ce5fafe0ec16ea6f11512
Chirag Thanki <149762309+chigs-thanky@users.noreply.github.com>
2025-01-09 14:17:25 +0530

    Array update

---
+++ Chapter 4/01_Array.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+
+int main()
+{
+    int myNum[] = {15, 20, 25, 30, 35, 40, 45, 50};
+    int i;
+
+    for (i = 0; i < 8; i++)
+    {
+        printf("%d\n", myNum[i]);
+    }
+    return 0;
+}
