738c49332be09b114a8a782ebd632209f0758a2d
Chirag Thanki <149762309+chigs-thanky@users.noreply.github.com>
2025-01-13 11:50:50 +0530

    Update Arrays

---
+++ 04 Array/03_String.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+#include <string.h>
+
+int main()
+{
+    char alphabet[] = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+    printf("Length = %d", strlen(alphabet));
+    printf("\nSize = %d", sizeof(alphabet));
+    return 0;
+}
