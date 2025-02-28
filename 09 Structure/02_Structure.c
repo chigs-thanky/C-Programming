9f183332b9c16fba48fa81368ada957dd0e7eec3
chigs-thanky <chigsthanky@outlook.com>
2025-03-01 00:00:27 +0530

    Modify structure data

--- 09 Structure/02_Structure.c
+++ 09 Structure/02_Structure.c
@@ -1,13 +1,6 @@
 #include <stdio.h>
 #include <string.h>
 
-struct myStructure
-{
-    int myNum;
-    char myLetter;
-    char myString[30]; // String
-};
-
 struct myDetails
 {
     char firstName[30];
@@ -17,12 +10,9 @@ struct myDetails
 
 int main()
 {
-    struct myStructure s1;
     struct myDetails c;
 
     // Assign a value to the string using the strcpy function
-    strcpy(s1.myString, "Some text");
-
     strcpy(c.firstName, "Chigs");
     strcpy(c.lastName, "Thanky");
     // strcpy(c.age, 27); //Gives Error because age is not a string
